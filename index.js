2be47ed29bcd4ec608bf4b4186bdfe6a0fe6266e
encredible <descartes131@gmail.com>
2021-09-01 18:29:48 +0900

    merng stack social media server

---
+++ index.js
@@ -0,0 +1,26 @@
+const { ApolloServer } = require('apollo-server');
+const { PubSub } = require('graphql-subscriptions');
+const mongoose = require('mongoose');
+
+const { MONGODB } = require('./config.js');
+
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers')
+
+const pubsub = new PubSub();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({req}) =>  ({ req, pubsub })
+});
+
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+      console.log(`MongoDB connected`)
+      return server.listen({port: 5000});
+  })
+  .then((res) => {
+      console.log(`Server running at ${res.url}`)
+  })

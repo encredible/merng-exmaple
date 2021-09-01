7e002cf085495162255128c586e5905f47ecc1bb
encredible <descartes131@gmail.com>
2021-09-01 19:22:40 +0900

    subscription 포기

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const { MONGODB } = require('./config.js');
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers')
 
+// TODO: subscription은 일단 포기.. 하는 방법이 너무 어려우니 나중에. apollo-server-express를 사용하라고 가이드가 되어 있음
+// https://www.apollographql.com/docs/apollo-server/data/subscriptions/
 const pubsub = new PubSub();
 
 const server = new ApolloServer({
